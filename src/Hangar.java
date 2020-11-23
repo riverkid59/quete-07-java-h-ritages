e34f7d2d1f0086e0dba131daa7f23a9a2c8846e2
riverkid59 <josselin.evrard@sncf.fr>
2020-11-23 16:55:06 +0100

    Quete 07 terminée

---
+++ src/Hangar.java
@@ -0,0 +1,13 @@
+
+public class Hangar {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+			Car myCar = new Car ( "360Modena", 2500);
+			Boat myBoat = new Boat ( "HorsBord", 150);
+			
+			System.out.println(myCar.doStuff());
+			System.out.println(myBoat.doStuff());
+	}
+}
---
+++ src/Vehicle.java
@@ -0,0 +1,28 @@
+
+public abstract class Vehicle {
+	private String brand;
+	private int kilometers;
+	
+	public Vehicle (String brand, int kilometers) {
+		this.brand = brand;
+		this.kilometers = kilometers;
+	}
+	
+	public String getBrand( ) {
+		return this.brand;
+	}
+	
+	public int kilometers() {
+		return this.kilometers;
+	}
+	
+	public void setbrand( String brand) {
+		this.brand = brand;
+	}
+	
+	public void setKilometers( int kilometers ) {
+		this.kilometers = kilometers;
+	}
+	
+	public abstract String doStuff();
+}
